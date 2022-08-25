ab378c2be7d216b298fa0e7962f57918d94563c6
KAVEESHA SITHUM <107849120+Kaveeshasithum@users.noreply.github.com>
2022-08-25 16:39:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@ global.APIKeys = {
 
 
 global.OWNER_REACT = '🧑🏻‍💻'
-global.REACT_NUMBER = '9476289854'
+global.REACT_NUMBER = '94774579505'
 
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
